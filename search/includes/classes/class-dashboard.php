a3ba76c3cfeaf5ad423a770783146bc06000c43f
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-02-03 12:21:06 -0700

    Remove network admin menu

--- search/includes/classes/class-dashboard.php
+++ search/includes/classes/class-dashboard.php
@@ -5,13 +5,17 @@ namespace Automattic\VIP\Search;
 class Dashboard {
 	public function __construct() {
 		add_action( 'admin_menu', '\ElasticPress\Dashboard\action_admin_menu' );
-		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ) );
+		add_action( 'network_admin_menu', array( __CLASS__, 'network_admin_menu' ) );
 		add_action( 'admin_bar_menu', array( __CLASS__, 'admin_bar_menu' ), 60 );
 	}
 
 	public function admin_menu() {
 		remove_submenu_page( 'elasticpress', 'elasticpress-settings' );
 		remove_submenu_page( 'elasticpress', 'elasticpress-health' );
+	}
+
+	public function network_admin_menu() {
+		remove_menu_page( 'elasticpress' );
 	}
 
 	/**
