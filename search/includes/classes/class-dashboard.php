b268d8661a747ccf518edde9333fba3ff1d1cc9f
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-02-03 15:36:17 -0700

    Hide unsupported features from Features page

--- search/includes/classes/class-dashboard.php
+++ search/includes/classes/class-dashboard.php
@@ -8,6 +8,19 @@ class Dashboard {
 		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ) );
 		add_action( 'network_admin_menu', array( __CLASS__, 'network_admin_menu' ) );
 		add_action( 'admin_bar_menu', array( __CLASS__, 'admin_bar_menu' ), 60 );
+		add_action( 'admin_print_styles', array( __CLASS__, 'hide_unsupported_features' ) );
+	}
+
+	public static function hide_unsupported_features() {
+		$screen = get_current_screen();
+		if ( 'toplevel_page_elasticpress' === $screen->base ) {
+			// Hide features that aren't supported from UI.
+			echo '<style>
+			div.ep-feature.ep-feature-autosuggest, div.ep-feature.ep-feature-documents, div.ep-feature.ep-feature-woocommerce {
+				display: none !important;
+			}
+			</style>';
+		}
 	}
 
 	public static function admin_menu() {
