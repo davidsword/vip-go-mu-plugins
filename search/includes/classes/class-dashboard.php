cb32146a4997a7d8499d505dafee9a09e4488dd3
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-02-03 11:38:05 -0700

    Search: add back menu UI

--- search/includes/classes/class-dashboard.php
+++ search/includes/classes/class-dashboard.php
@@ -4,27 +4,21 @@ namespace Automattic\VIP\Search;
 
 class Dashboard {
 	public function __construct() {
+		add_action( 'admin_menu', '\ElasticPress\Dashboard\action_admin_menu' );
 		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ) );
-		add_action( 'network_admin_menu', array( __CLASS__, 'admin_menu' ) );
 		add_action( 'admin_bar_menu', array( __CLASS__, 'admin_bar_menu' ), 60 );
 	}
 
-	/**
-	 * Disable the Elasticpress Dashboard links
-	 */
-	public static function admin_menu() {
-		if ( ! is_automattician() ) {
-			remove_menu_page( 'elasticpress' );
-		}
+	public function admin_menu() {
+		remove_submenu_page( 'elasticpress', 'elasticpress-settings' );
+		remove_submenu_page( 'elasticpress', 'elasticpress-health' );
 	}
 
 	/**
 	 * Disable the Elasticpress Network Dashboard link
 	 */
 	public static function admin_bar_menu( $admin_bar ) {
-		if ( ! is_automattician() ) {
-			$admin_bar->remove_menu( 'network-admin-elasticpress' );
-		}
+		$admin_bar->remove_menu( 'network-admin-elasticpress' );
 	}
 }
 
